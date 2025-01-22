03c86febe789305b06f76eea4fbe25308db963c8
Lev Neiman <lev.neiman@gmail.com>
2025-01-21 18:17:46 -0800

    fixups

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="celery-redis-cluster",
-    version="0.1.2",
+    version="0.1.5",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
